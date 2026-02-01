ae18625c1780e25e610f5211f9beb0a4466d2c45
TOMLUCA7 <your-github-email@example.com>
2026-02-01 17:15:30 +0200

    Refactor UserModel.js to change exported functions to local scope for improved encapsulation and update getRandomPokemon to fetch a random Pokémon by ID, returning structured data.

--- js/model/UserModel.js
+++ js/model/UserModel.js
@@ -6,7 +6,7 @@ const GET_POKEMON_API_URL = "https://pokeapi.co/api/v2/pokemon/";
 const GET_QUOTE_API_URL = "https://api.kanye.rest";
 const GET_ABOUT_ME_TEXT = "https://baconipsum.com/api/?type=meat-and-filler";
 
-export const getRandomUser = async () => {
+const getRandomUser = async () => {
   try {
     const response = await fetch(`${USER_API_URL}`);
     if (!response.ok) throw new Error("Faild to get USER API");
@@ -17,18 +17,22 @@ export const getRandomUser = async () => {
   }
 };
 
-export const getRandomPokemon = async () => {
+const getRandomPokemon = async () => {
   try {
-    const response = await fetch(`${GET_POKEMON_API_URL}`);
+    const randomPokemonId = Math.floor(Math.random() * 898) + 1;
+    const response = await fetch(`${GET_POKEMON_API_URL}${randomPokemonId}`);
     if (!response.ok) throw new Error("Faild to get GET POKEMON API");
     const data = await response.json();
-    return data.results[0];
+    return {
+      name: data.name,
+      img: data.url,
+    };
   } catch (error) {
     throw new Error("Failed to get random pokemon");
   }
 };
 
-export const getRandomQuote = async () => {
+const getRandomQuote = async () => {
   try {
     const response = await fetch(`${GET_QUOTE_API_URL}`);
     if (!response.ok) throw new Error("Faild to get GET QUOTE API");
@@ -39,7 +43,7 @@ export const getRandomQuote = async () => {
   }
 };
 
-export const getAboutMeText = async () => {
+const getAboutMeText = async () => {
   try {
     const response = await fetch(`${GET_ABOUT_ME_TEXT}`);
     if (!response.ok) throw new Error("Faild to get GET ABOUT ME TEXT API");
